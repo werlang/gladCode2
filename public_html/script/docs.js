b084f613f7a5dd3e8ef14b3bfc5d8132b40a6d7d
Pablo Werlang <pswerlang@gmail.com>
2019-04-23 20:31:04 -0300

    2.5.3

--- public_html/script/docs.js
+++ public_html/script/docs.js
@@ -1,6 +1,13 @@
 $(document).ready( function(){
     $('#side-menu').load("docs-menu.html", function(){
+		var icon = "<i class='material-icons'>arrow_forward_ios</i>";
+		$('#side-menu li').each( function(){
+			if ($(this).next('ul').length != 0)
+				$(this).prepend(icon);
+		});
+		
         $('#side-menu li').hide();
+        $('#side-menu li i').removeClass('open');
         $('#side-menu > ul > li').show();
         
         $('#side-menu #search input').on('input', function(){
@@ -11,22 +18,32 @@ $(document).ready( function(){
             }
             else{
                 var pattern = new RegExp("[\\w]*"+ text +"[\\w]*","ig");
-                $.each($('#side-menu li'), function(index,item){
-                    if ($(item).text().match(pattern)){
-                        $(item).show();
-                        $(item).parent().prev('li').show().parent().prev('li').show();
+                $('#side-menu li').each(function(){
+                    if ($(this).text().match(pattern)){
+                        $(this).show();
+                        $(this).parent().prev('li').show().parent().prev('li').show();
                     }
                 });
+                $('#side-menu li').each(function(){
+					if ($(this).css('display') != 'none')
+						$(this).children('i').addClass('open');
+					else
+						$(this).children('i').removeClass('open');
+				});
             }
         });
 
         $('#side-menu a').click( function(e){
             e.preventDefault();
             var list = $(this).parent().next('ul');
-            if (list.children().css('display') != 'none')
-                list.find('li').hide();
-            else
-                list.children('li').show();
+            if (list.children().css('display') != 'none'){
+                list.find('li').slideUp();
+				$(this).prev('i').removeClass('open');
+			}
+            else{
+                list.children('li').slideDown();
+				$(this).prev('i').addClass('open');
+			}
         });
     });
 });
