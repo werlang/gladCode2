9af34e94e2fb5f135c76e382fa576a15f211d92a
root <root@localhost.localdomain>
2019-12-10 06:58:32 -0300

    2.7.1 - minor bug fixes

--- public_html/post.php
+++ public_html/post.php
@@ -11,6 +11,7 @@
 <head>
     <meta charset='utf-8' />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/gif" href="icon/gladcode_icon.png" />
     <BASE href="../">
 	<link rel="icon" type="image/gif" href="icon/gladcode_icon.png" />
 	<title>gladCode - Publicação</title>
