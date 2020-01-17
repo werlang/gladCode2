eabbc73a082493e00bde85fb291307fe09839343
root <root@localhost.localdomain>
2020-01-17 06:10:35 -0300

    2.8 - added blockly - changed to local scripts

---
+++ public_html/script/blocks.js
@@ -0,0 +1,315 @@
+Blockly.Blocks['loop'] = {
+	init: function() {
+		this.appendDummyInput()
+			.appendField("loop");
+		this.appendStatementInput("CONTENT");
+		this.setColour("#00638d");
+        this.setTooltip("Função que o gladiador irá executar a cada 0.1s");
+        this.setHelpUrl("manual");
+        this.setDeletable(false);
+	}
+};
+
+Blockly.Python['loop'] = function(block) {
+    var code = Blockly.Python.statementToCode(block, 'CONTENT');
+    if (code == "")
+        code = "  pass";
+	return `def loop():\n${code}`;
+};
+
+Blockly.Blocks['ignore_return'] = {
+	init: function() {
+		this.appendValueInput("function")
+		this.setInputsInline(false);
+		this.setPreviousStatement(true, null);
+		this.setNextStatement(true, null);
+		this.setColour(230);
+        this.setTooltip("Ignora o retorno de uma função");
+        this.setHelpUrl("");
+	}
+};
+
+Blockly.Python['ignore_return'] = function(block) {
+	var value_function = Blockly.Python.valueToCode(block, 'function', Blockly.Python.ORDER_ATOMIC || '0');
+	var code = `${value_function}\n`;
+	return code;
+};
+
+Blockly.Blocks['move'] = {
+	init: function() {
+		this.appendDummyInput()
+			.appendField("Mover para")
+			.appendField(new Blockly.FieldDropdown([["Posição","TO"], ["Alvo","TARGET"]], this.selection.bind(this)), "COMPLEMENT");
+		this.setInputsInline(true);
+		this.setOutput(true, "Boolean");
+		this.setColour(210);
+		this.setTooltip("tooltip");
+		this.setHelpUrl("url");
+		this.columnType = this.getFieldValue('COMPLEMENT');
+		this.reshape(true);
+	},
+	mutationToDom: function() {
+		var container = document.createElement('mutation');
+		var hasposition = "false";
+		if (this.getFieldValue('COMPLEMENT') == "TO")
+			hasposition = "true";
+		container.setAttribute('position', hasposition);
+		return container;
+	},
+	domToMutation: function(xmlElement) {
+		var hasposition = (xmlElement.getAttribute('position') == "true");
+		this.reshape(hasposition);
+	},
+	reshape(hasposition){
+		if (this.getInput('X') && !hasposition){
+			this.removeInput('X');
+			this.removeInput('Y');
+		}
+		else if (!this.getInput('X') && hasposition){
+			this.appendValueInput("X")
+				.setCheck("Number")
+				.setAlign(Blockly.ALIGN_RIGHT)
+				.appendField("X");
+			this.appendValueInput("Y")
+				.setCheck("Number")
+				.setAlign(Blockly.ALIGN_RIGHT)
+				.appendField("Y");
+		}
+	},
+	selection: function (option) {
+		if (option == "TO")
+			this.reshape(true);
+		else
+			this.reshape(false);
+	},
+};
+
+Blockly.Python['move'] = function(block) {
+	var dropdown_complement = block.getFieldValue('COMPLEMENT');
+	var value_x = (Blockly.Python.valueToCode(block, 'X', Blockly.Python.ORDER_ATOMIC) || 0);
+	var value_y = (Blockly.Python.valueToCode(block, 'Y', Blockly.Python.ORDER_ATOMIC) || 0);
+
+	var code = `moveToTarget()`;
+	if (dropdown_complement == "TO")
+		code = `moveTo(${value_x}, ${value_y})`;
+
+	return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Blocks['step'] = {
+	init: function() {
+		this.appendDummyInput()
+			.appendField("Passo para")
+			.appendField(new Blockly.FieldDropdown([["Frente","FORWARD"], ["Trás","BACK"], ["Esquerda","LEFT"], ["Direita","RIGHT"]]), "COMPLEMENT");
+		this.setInputsInline(true);
+		this.setOutput(true, "Number");
+		this.setColour(210);
+		this.setTooltip("tooltip");
+		this.setHelpUrl("url");
+	},
+};
+
+Blockly.Python['step'] = function(block) {
+	var dropdown_complement = block.getFieldValue('COMPLEMENT');
+	var values = {
+		FORWARD: "Forward",
+		BACK: "Back",
+		LEFT: "Left",
+		RIGHT: "Right"
+	};
+
+	code = `step${values[dropdown_complement]}()`;
+
+	return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Blocks['moveforward'] = {
+	init: function() {
+		this.appendDummyInput()
+			.appendField("Mover para frente")
+		this.appendValueInput("STEPS")
+			.setCheck("Number")
+			.setAlign(Blockly.ALIGN_RIGHT)
+		this.appendDummyInput()
+			.appendField("passos");
+		this.setInputsInline(true);
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+		this.setColour(210);
+		this.setTooltip("tooltip");
+		this.setHelpUrl("url");
+	},
+};
+
+Blockly.Python['moveforward'] = function(block) {
+	var steps = (Blockly.Python.valueToCode(block, 'STEPS', Blockly.Python.ORDER_ATOMIC) || 0);
+	code = `moveForward(${steps})\n`;
+
+	return code;
+};
+
+Blockly.Blocks['turn'] = {
+	init: function() {
+		this.appendDummyInput()
+			.appendField("Virar para")
+			.appendField(new Blockly.FieldDropdown([["Posição","TO"], ["Alvo","TARGET"], ["Ataque recebido","HIT"], ["Esquerda","LEFT"], ["Direita","RIGHT"]], this.selection.bind(this)), "COMPLEMENT");
+		this.setInputsInline(true);
+		this.setOutput(true, "Boolean");
+		this.setColour(210);
+		this.setTooltip("tooltip");
+		this.setHelpUrl("url");
+		// this.option = null;
+		this.reshape(true);
+	},
+	mutationToDom: function() {
+		var container = document.createElement('mutation');
+		var option = this.getFieldValue('COMPLEMENT');
+		container.setAttribute('where', option);
+		return container;
+	},
+	domToMutation: function(xmlElement) {
+		var option = xmlElement.getAttribute('where');
+		this.reshape(option);
+	},
+	reshape(option){
+		var input_now = [];
+		for (let i in this.inputList){
+			let name = this.inputList[i].name;
+			if (name != "")
+				input_now.push(name);
+		}
+
+		var input_next = [];
+		if (option == "TO"){
+			input_next.push("X", "Y");
+		}
+		else if (["LEFT", "RIGHT"].indexOf(option) != -1){
+			input_next.push("ANGLE", "TEXT");
+		}
+		else if (option == "TARGET"){
+		}
+
+		// remove uneeded input
+		for (let i in input_now){
+			if (input_next.indexOf(input_now[i]) == -1){
+				this.removeInput(input_now[i]);
+			}
+		}
+
+		// include needed inputs
+		for (let i in input_next){
+			if (input_next[i] == "X" && !this.getInput("X")){
+				this.appendValueInput("X")
+					.setCheck("Number")
+					.setAlign(Blockly.ALIGN_RIGHT)
+					.appendField("X");
+			}
+			else if (input_next[i] == "Y" && !this.getInput("Y")){
+				this.appendValueInput("Y")
+					.setCheck("Number")
+					.setAlign(Blockly.ALIGN_RIGHT)
+					.appendField("Y");
+			}
+			else if (input_next[i] == "ANGLE" && !this.getInput("ANGLE")){
+				this.appendValueInput("ANGLE")
+					.setCheck("Number")
+				this.appendDummyInput("TEXT")
+					.appendField("Graus")
+					.setAlign(Blockly.ALIGN_RIGHT);
+			}
+		}
+	},
+	selection: function (option) {
+		this.reshape(option);
+	},
+};
+
+Blockly.Python['turn'] = function(block) {
+	var dropdown_where = block.getFieldValue('COMPLEMENT');
+	var values = {
+		TO: "To",
+		LEFT: "Left",
+		RIGHT: "Right",
+		TARGET: "ToTarget",
+		HIT: "ToLastHit"
+	};
+
+	var code = `turn${values[dropdown_where]}`;
+
+	if (dropdown_where == "TO"){
+		var value_x = (Blockly.Python.valueToCode(block, 'X', Blockly.Python.ORDER_ATOMIC) || 0);
+		var value_y = (Blockly.Python.valueToCode(block, 'Y', Blockly.Python.ORDER_ATOMIC) || 0);
+		code += `(${value_x}, ${value_y})`;
+	}
+	else if (dropdown_where == "LEFT" || dropdown_where == "RIGHT"){
+		var value_angle = (Blockly.Python.valueToCode(block, 'ANGLE', Blockly.Python.ORDER_ATOMIC) || 0);
+		code += `(${value_angle})`;
+	}
+	else{
+		code += "()";
+	}
+
+	return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Blocks['turnangle'] = {
+	init: function() {
+		this.appendValueInput("ANGLE")
+			.setCheck("Number")
+			.appendField("Virar");
+		this.appendDummyInput()
+			.appendField("Graus relativo")
+			.appendField(new Blockly.FieldDropdown([["ao gladiador","TURN"], ["à arena","ANGLE"]], this.selection.bind(this)), "COMPLEMENT")
+			.setAlign(Blockly.ALIGN_RIGHT);
+		this.setOutput(false);
+		this.setNextStatement(true);
+		this.setPreviousStatement(true);
+		this.setColour(210);
+		this.setTooltip("tooltip");
+		this.setHelpUrl("url");
+		// this.reshape(true);
+	},
+	mutationToDom: function() {
+		var container = document.createElement('mutation');
+		var option = this.getFieldValue('COMPLEMENT');
+		container.setAttribute('operation', option);
+		return container;
+	},
+	domToMutation: function(xmlElement) {
+		var option = xmlElement.getAttribute('operation');
+		this.reshape(option);
+	},
+	reshape(option){
+		this.unplug();
+		if (option == "TURN"){
+			this.setOutput(false);
+			this.setNextStatement(true);
+			this.setPreviousStatement(true);
+		}
+		else{
+			this.setNextStatement(false);
+			this.setPreviousStatement(false);
+			this.setOutput(true, "Boolean");
+		}
+	},
+	selection: function (option) {
+		this.reshape(option);
+	},
+};
+
+Blockly.Python['turnangle'] = function(block) {
+	var value_angle = (Blockly.Python.valueToCode(block, 'ANGLE', Blockly.Python.ORDER_ATOMIC) || 0);
+	var dropdown_op = block.getFieldValue('COMPLEMENT');
+
+	var code = "turn";
+
+	if (dropdown_op == "ANGLE")
+		code += "ToAngle";
+
+	code += `(${value_angle})`;
+
+	if (dropdown_op == "ANGLE")
+		return [code, Blockly.Python.ORDER_NONE];
+	else
+		return code;
+};
