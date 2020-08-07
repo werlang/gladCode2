a0531ac36e10694f420ca26119f9d9fc779cfb10
root <root@local.gladcode.dev>
2020-08-07 12:18:52 -0300

    Changes for nginx server

--- node/server.js
+++ node/server.js
@@ -57,6 +57,7 @@ app.use(cors({
     origin: [
         'http://127.0.0.1:85',
         'http://127.0.0.1',
+        'http://127.0.0.1:8181',
         'http://gladcode.tk',
         'https://gladcode.tk',
         'http://www.gladcode.tk',
