0c42e39baa3e8008d06889598f09d917a547b053
Pablo Werlang <pswerlang@gmail.com>
2026-02-04 23:57:49 +0000

    refactor(remove_old_battles): Update file paths and improve code structure

--- script/remove_old_battles.php
+++ script/remove_old_battles.php
@@ -1,26 +1,36 @@
 <?php
-    include_once "/home/gladcode/public_html/connection.php";
+$path = "/var/www/html";
 
-    $modes = array(
-        "editor" => "1 WEEK",
-        "ranked" => "1 YEAR"
-    );
+include_once "$path/connection.php";
 
-    foreach($modes as $mode => $period){
-        $fav = $mode == "ranked" ? " AND id NOT IN (SELECT DISTINCT log FROM reports WHERE favorite = 1)" : "";
-        $sql = "SELECT id FROM logs WHERE origin = '$mode' AND time < now() - INTERVAL $period AND expired = 0 $fav";
+$modes = array(
+    "editor" => "1 WEEK",
+    "ranked" => "1 YEAR",
+);
+    
+$deleteFromLogsAfter = "3 YEAR";
+
+foreach($modes as $mode => $period){
+    $fav = $mode == "ranked" ? " AND id NOT IN (SELECT DISTINCT log FROM reports WHERE favorite = 1)" : "";
+    $sql = "SELECT id FROM logs WHERE origin = '$mode' AND time < now() - INTERVAL $period AND expired = 0 $fav";
+    $result = runQuery($sql);
+
+    echo "Removing old $mode battles... " . $result->rowCount() . " battles found.\n";
+    
+    if ($result->rowCount() > 0){
+        $ids = array();
+        while($row = $result->fetch()){
+            $id = $row['id'];
+            if (file_exists("$path/logs/$id")) {
+                unlink("$path/logs/$id");
+            }
+            array_push($ids, $id);
+        }		
+        $ids = implode(",", $ids);
+        $sql = "UPDATE logs SET expired = 1 WHERE id IN ($ids)";
         $result = runQuery($sql);
-        
-        if ($result->rowCount() > 0){
-            $ids = array();
-            while($row = $result->fetch()){
-                $id = $row['id'];
-                unlink("/home/gladcode/public_html/logs/$id");
-                array_push($ids, $id);
-            }		
-            $ids = implode(",", $ids);
-            $sql = "UPDATE logs SET expired = 1 WHERE id IN ($ids)";
-            $result = runQuery($sql);
-        }
+        $sql = "DELETE FROM logs WHERE id IN ($ids) AND origin = '$mode' AND time < now() - INTERVAL $deleteFromLogsAfter";
+        $result = runQuery($sql);
+        echo "Deleted " . $result->rowCount() . " records from logs table.\n";
     }
-?>
+}
